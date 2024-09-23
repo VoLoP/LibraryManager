69b38f1b548ceb436c056db03d7e7430e0b6b9ad
Simone <bativolpet1@gmail.com>
2024-09-24 01:12:45 +0200

    Added a Json to save the books
    Added the logic to save the existing books in a Json

--- LibraryManager/LibraryManager.cs
+++ LibraryManager/LibraryManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,12 @@ namespace LibraryManager
     {
         private List<Book> books = new List<Book>();
         private int nextId = 1;
+        private const string FilePath = "books.json";
+
+        public LibraryManager()
+        {
+            LoadBooks();
+        }
 
         public void AddBook(string title, string author)
         {
@@ -20,6 +28,7 @@ namespace LibraryManager
                 Author = author
             };
             books.Add(book);
+            SaveBooks();
             Console.WriteLine($"Book '{title}' by {author} added with ID {book.Id}.");
         }
 
@@ -29,6 +38,7 @@ namespace LibraryManager
             if (book != null)
             {
                 books.Remove(book);
+                SaveBooks();
                 Console.WriteLine($"Book with ID {id} removed.");
             }
             else
@@ -44,6 +54,7 @@ namespace LibraryManager
             {
                 book.Title = newTitle;
                 book.Author = newAuthor;
+                SaveBooks();
                 Console.WriteLine($"Book with ID {id} updated to '{newTitle}' by {newAuthor}.");
             }
             else
@@ -63,6 +74,25 @@ namespace LibraryManager
                 foreach (var book in books)
                 {
                     Console.WriteLine($"ID: {book.Id}, Title: {book.Title}, Author: {book.Author}");
+                }
+            }
+        }
+
+        private void SaveBooks()
+        {
+            var json = JsonConvert.SerializeObject(books);
+            File.WriteAllText(FilePath, json);
+        }
+
+        private void LoadBooks()
+        {
+            if (File.Exists(FilePath))
+            {
+                var json = File.ReadAllText(FilePath);
+                books = JsonConvert.DeserializeObject<List<Book>>(json);
+                if (books.Count > 0)
+                {
+                    nextId = books.Max(b => b.Id) + 1;
                 }
             }
         }
